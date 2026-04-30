f7a2a2573ad109ee74f76bbb295c91d72718ad25
Sleiman <sleimanelhajj7@gmail.com>
2026-04-30 21:35:38 +0300

    fixed sso call back

--- Frontend/src/environments/environment.development.ts
+++ Frontend/src/environments/environment.development.ts
@@ -1,6 +1,7 @@
 export const environment = {
   production: false,
+  frontendUrl: 'http://localhost:4200',
   apiUrl: 'http://localhost:3000',
   supabaseUrl: 'https://qwjvrkhisnctmsebaxfh.supabase.co',
-  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3anZya2hpc25jdG1zZWJheGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NzA2MzAsImV4cCI6MjA4ODQ0NjYzMH0.m0-IxHFReTJWJb2bqd4GHMMn6ZtM6tJJ7ND7_KdLiQs', // replace with anon key from Supabase dashboard → Settings → API
+  supabaseAnonKey: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InF3anZya2hpc25jdG1zZWJheGZoIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI4NzA2MzAsImV4cCI6MjA4ODQ0NjYzMH0.m0-IxHFReTJWJb2bqd4GHMMn6ZtM6tJJ7ND7_KdLiQs',
 };
